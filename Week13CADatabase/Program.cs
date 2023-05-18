6684feab0cf679edbd4f44bc860ca6827ff5a9a2
caprxquemini <stefaniabarabula12@gmail.com>
2023-05-18 13:33:04 +0300

    Add project files.

---
+++ Week13CADatabase/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SQLite;
+
+
+static SQLiteConnection CreateConnection ()
+{
+    SQLiteConnection connection = new SQLiteConnection("Data Source=mydb.db; Version=3; New=true; Compress = true");
+
+    try
+{
+    connection.Open();
+    Console.WriteLine("Connection established");
+} catch
+{
+    Console.WriteLine("DB connection failed");
+}
+
+    try
+    {
+        connection.Open();
+        Console.WriteLine("Connection established");
+    }
+    catch
+    {
+        Console.WriteLine("DB connection failed");
+    }
+
+    return connection;
+}
